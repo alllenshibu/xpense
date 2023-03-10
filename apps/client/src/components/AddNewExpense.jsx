51ead3b969f9c2b948236a529471cadc779f71b4
vaidyanath b <b.vaidyanath003@gmail.com>
2023-03-10 16:47:50 +0530

    axios post expense

--- apps/client/src/components/AddNewExpense.jsx
+++ apps/client/src/components/AddNewExpense.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 const categories = ["food","transport","entertainment","shopping","bills","other"];
 const friends = ["bot17","helloman","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 const friendsoptions = friends.map((friend) => {
@@ -28,6 +29,19 @@ function Log () {
       console.log(displayCategory);
       console.log(displaySplit);
       console.log(splitError);
+
+      axios.post(`${import.meta.env.VITE_API_URL}/expense/add`,{
+                  user : "bot17",
+                  expense : {
+                      name : log.name,
+                      amount : log.amount,
+                      category : log.category,
+                      date : log.date,
+                      split : split,
+                      group : group,
+                      splitEqual : splitEqual
+                  }
+                })
     
     }
     
