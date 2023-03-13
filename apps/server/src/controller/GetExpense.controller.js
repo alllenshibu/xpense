f3bbb861dfbb5a69c86230259df1457583e147c0
vaidyanath b <b.vaidyanath003@gmail.com>
2023-03-13 09:52:45 +0530

    fix

--- apps/server/src/controller/GetExpense.controller.js
+++ apps/server/src/controller/GetExpense.controller.js
@@ -1,8 +1,14 @@
 const {getAllExpenses} = require("../services/expense.service.js")
 const {getAllShares} = require("../services/shares.service.js")
-
+const {pool} = require("../config/postgres.config.js")
 const getAllExpensesController = async (req, res) => {
-    const user = req.body.user_id
+    const username = req.body.username
+
+    const user = await pool.query("SELECT user_id FROM users WHERE username = $1;", [username]).then((response) => {
+        return response.rows[0].user_id
+    })
+
+
     const expenses = await getAllExpenses(user)
     console.log("Expenses: " + expenses)
     const shares = await getAllShares(user)
