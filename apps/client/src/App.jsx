8e84c842483f01801cfd0918ad136076c6210df3
Allen Shibu <93600615+alapanoski@users.noreply.github.com>
2023-03-19 16:50:57 +0530

    Remove unneccessary files

--- apps/client/src/App.jsx
+++ apps/client/src/App.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 
-import './App.css';
 import AddNewExpense from './components/AddNewExpense';
 import Auth from './components/Auth';
 import ExpenseExplorer from './components/ExpenseExplorer';
