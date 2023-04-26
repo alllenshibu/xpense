92de3f7b5d7499a542573c4f498f2a0a2fc4ee47
vaidyanath b <b.vaidyanath003@gmail.com>
2023-04-26 23:28:43 +0530

    auth removed fn

--- apps/server/src/middleware/verifyToken.js
+++ apps/server/src/middleware/verifyToken.js
@@ -1,31 +1,32 @@
 const jwt = require('jsonwebtoken');
 const verifyToken = (req, res, next) => {
 
-    const path = req.path.toLowerCase();
-    if (path === '/login' || path === '/register') {
-        return next();
-    }
-    console.log(req.path) //token authorization bearer
+    // const path = req.path.toLowerCase();
+    // if (path === '/login' || path === '/register') {
+    //     return next();
+    // }
+    // console.log(req.path) //token authorization bearer
     
-    try{
-      const token = req.headers.authorization.split(' ')[1];
-      if (!token) {
-        const error = new Error('Not authenticated');
-        error.statusCode = 401;
-        throw error;
-      }
-      const decodedToken = jwt.verify(token, process.env.SECRETKEY);
-      if (!decodedToken) {
-        const error = new Error('Not authenticated');
-        error.statusCode = 401;
-        throw error;
-      }
-      req.username = decodedToken.username;
-      next();
-    }catch(err){
+    // try{
+    //   const token = req.headers.authorization.split(' ')[1];
+    //   if (!token) {
+    //     const error = new Error('Not authenticated');
+    //     error.statusCode = 401;
+    //     throw error;
+    //   }
+    //   const decodedToken = jwt.verify(token, process.env.SECRETKEY);
+    //   if (!decodedToken) {
+    //     const error = new Error('Not authenticated');
+    //     error.statusCode = 401;
+    //     throw error;
+    //   }
+    //   req.username = decodedToken.username;
+    //   next();
+    // }catch(err){
     
-      res.status(401).json({ message: err.message });
-    }
+    //   res.status(401).json({ message: err.message });
+    // }
+    next();
   };
 
   module.exports = verifyToken;
