ff6f88276818926a84842abd9f3289c635d74931
Allen Shibu <93600615+alapanoski@users.noreply.github.com>
2023-04-30 12:14:47 +0530

    Reorganisation

--- apps/client/src/pages/friendPage/Friends.jsx
+++ apps/client/src/pages/friendPage/Friends.jsx
@@ -1,12 +1,11 @@
 import React, { useEffect, useState } from 'react';
 
 import axios from 'axios';
-import  Left from './Left/Left';
-import Right from './Right/Right';
+import Left from './Left/Left';
+import FriendBar from './Right/friendBar';
+import RequestBar from './Left/RequestBar';
 
 const Friends = ({ user }) => {
-  const [friends, setFriends] = useState([]);
-
   // useEffect(() => {
   //   axios
   //     .get(`${import.meta.env.VITE_API_URL}/friends/getFriends/${user.username}`, {
@@ -19,13 +18,106 @@ const Friends = ({ user }) => {
   //     });
   // }, []);
 
+  const [friends, setFriends] = useState([]);
+  const username = JSON.parse(localStorage.getItem('user')).username;
+  useEffect(() => {
+    axios
+      .get(`${import.meta.env.VITE_API_URL}/friends/getFriends/${username}`, {
+        withCredentials: true,
+      })
+      .then((res) => {
+        console.log(res.data);
+        setFriends(res.data);
+      });
+  }, []);
+
+  const [friendName, setFriendName] = useState('');
+
+  const handleAddFriend = () => {
+    axios
+      .post(`${import.meta.env.VITE_API_URL}/friends/send`, {
+        username: username,
+        reciever: friendName,
+      })
+      .then((res) => {
+        console.log(res.data);
+      });
+  };
+
+  const [requests, setRequests] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchRequests = async () =>
+      axios
+        .get(`${import.meta.env.VITE_API_URL}/friends/getRequests/user1`, {
+          withCredentials: true,
+        })
+        .then((res) => {
+          console.log(res.data);
+          setRequests(res.data);
+        });
+    fetchRequests()
+      .then(() => setLoading(false))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div>
       <p className="text-4xl font-semibold">Friends</p>
-      
+
       <div className="w-full h-full flex">
-          <Left/> 
-          <Right/>
+        <div>
+          <div className="w-full h-full flex flex-col justify-center items-center gap-8">
+            <p className="text-4xl font-semibold">Add Friend</p>
+            <div className="flex">
+              <input
+                type="text"
+                placeholder="Friend's Username"
+                onChange={(e) => {
+                  setFriendName(e.target.value);
+                }}
+              />
+              <button type="submit" className="btn btn-primary" onClick={handleAddFriend}>
+                Add Friend
+              </button>
+            </div>
+          </div>
+        </div>
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          <div className="w-full h-full flex flex-col justify-center items-center gap-8">
+            <p
+              className="text-4xl font-semibold border-b-2 border-gray-300 pb-2
+        "
+            >
+              Incoming Requests
+            </p>
+            {requests.incoming &&
+              requests.incoming.map((request) => (
+                <RequestBar friendId={request.id} friendName={request.name} type="received" />
+              ))}
+            <p
+              className="text-4xl font-semibold border-b-2 border-gray-300 pb-2
+        "
+            >
+              Outgoing Requests
+            </p>
+            {requests.outgoing &&
+              requests.outgoing.map((request) => (
+                <RequestBar friendId={request.id} friendName={request.name} type="sent" />
+              ))}
+          </div>
+        )}
+        <div className="w-full h-full flex flex-col justify-center items-center gap-8">
+          <p className="text-4xl font-semibold border-b-2 ">Friends</p>
+          <div className="w-full h-full flex flex-col justify-center items-center gap-8">
+            {friends.map((friend) => {
+              return <FriendBar friend={friend} />;
+            })}
+          </div>
+        </div>
       </div>
     </div>
   );
