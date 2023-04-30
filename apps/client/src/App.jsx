e8228a36797b255c64691824e5db26ceb5a99b6b
Allen Shibu <93600615+alapanoski@users.noreply.github.com>
2023-04-30 12:06:42 +0530

    Login/logout issues fixed

--- apps/client/src/App.jsx
+++ apps/client/src/App.jsx
@@ -29,47 +29,49 @@ function App() {
         </div>
       )}
       {user && (
-        <div className="mx-auto h-full relative flex flex-row justify-center  bg-[var(--primarybg)] items-center">
-          <div className="w-96 h-full justify-self-start">
-            <Sidebar user={user} setUser={setUser} setFocusedTab={setFocusedTab} />
-          </div>
-          <div className="w-full h-full bg-[var(--bg-background)] flex flex-col justify-start items-center">
-            <Overview />
-            <div className="w-full h-full overflow-y-scroll flex flex-col px-2 justify-start">
-              {focusedTab === 'dashboard' && <ExpenseExplorer user={user} />}
-              {focusedTab === 'friends' && <Friends user={user} />}
+        <>
+          <div className="mx-auto h-full relative flex flex-row justify-center  bg-[var(--primarybg)] items-center">
+            <div className="w-96 h-full justify-self-start">
+              <Sidebar user={user} setUser={setUser} setFocusedTab={setFocusedTab} />
             </div>
-          </div>
-          {/* <div className="w-96 h-full bg-orange-300 flex flex-col justify-start items-center">
+            <div className="w-full h-full bg-[var(--bg-background)] flex flex-col justify-start items-center">
+              <Overview />
+              <div className="w-full h-full overflow-y-scroll flex flex-col px-2 justify-start">
+                {focusedTab === 'dashboard' && <ExpenseExplorer user={user} />}
+                {focusedTab === 'friends' && <Friends user={user} />}
+              </div>
+            </div>
+            {/* <div className="w-96 h-full bg-orange-300 flex flex-col justify-start items-center">
             <Calendar />
           </div> */}
-          <div
-            className="absolute bottom-20 right-40"
-            onClick={() => {
-              setIsPopupOpen(true);
-            }}
-          >
-            <PlusButton />
+            <div
+              className="absolute bottom-20 right-40"
+              onClick={() => {
+                setIsPopupOpen(true);
+              }}
+            >
+              <PlusButton />
+            </div>
           </div>
-        </div>
-      )}
 
-      <div
-        className={
-          (isPopupOpen ? 'fixed' : 'hidden') +
-          ' top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 bg-gray-200 rounded shadow-xl flex flex-col justify-center items-center'
-        }
-      >
-        <div
-          className="absolute -top-10 -right-10 rotate-45"
-          onClick={() => {
-            setIsPopupOpen(false);
-          }}
-        >
-          <PlusButton className="absolute top-0" />
-        </div>
-        <AddNewExpense user={user} />
-      </div>
+          <div
+            className={
+              (isPopupOpen ? 'fixed' : 'hidden') +
+              ' top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 bg-gray-200 rounded shadow-xl flex flex-col justify-center items-center'
+            }
+          >
+            <div
+              className="absolute -top-10 -right-10 rotate-45"
+              onClick={() => {
+                setIsPopupOpen(false);
+              }}
+            >
+              <PlusButton className="absolute top-0" />
+            </div>
+            <AddNewExpense user={user} />
+          </div>
+        </>
+      )}
     </div>
   );
 }
