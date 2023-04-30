ac1910305966fe7ee4878ab4ca227305ae50e842
vaidyanath b <b.vaidyanath003@gmail.com>
2023-04-30 13:06:06 +0530

    cleaned

--- apps/client/src/pages/Friends/Friends.jsx
+++ apps/client/src/pages/Friends/Friends.jsx
@@ -2,20 +2,10 @@ import React, { useEffect, useState } from 'react';
 
 import axios from 'axios';
 import FriendBar from './friendBar';
-import RequestBar from './RequestBar';
+import Requests from './Requests';
+
 
 const Friends = ({ user }) => {
-  // useEffect(() => {
-  //   axios
-  //     .get(`${import.meta.env.VITE_API_URL}/friends/getFriends/${user.username}`, {
-
-  //       withCredentials: true,
-  //     })
-  //     .then((res) => {
-  //       console.log(res.data);
-  //       setFriends(res.data);
-  //     });
-  // }, []);
 
   const [friends, setFriends] = useState([]);
   const username = JSON.parse(localStorage.getItem('user')).username;
@@ -30,36 +20,6 @@ const Friends = ({ user }) => {
       });
   }, []);
 
-  const [friendName, setFriendName] = useState('');
-
-  const handleAddFriend = () => {
-    axios
-      .post(`${import.meta.env.VITE_API_URL}/friends/send`, {
-        username: username,
-        reciever: friendName,
-      })
-      .then((res) => {
-        console.log(res.data);
-      });
-  };
-
-  const [requests, setRequests] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchRequests = async () =>
-      axios
-        .get(`${import.meta.env.VITE_API_URL}/friends/getRequests/user1`, {
-          withCredentials: true,
-        })
-        .then((res) => {
-          console.log(res.data);
-          setRequests(res.data);
-        });
-    fetchRequests()
-      .then(() => setLoading(false))
-      .catch((err) => console.log(err));
-  }, []);
 
   return (
     <div className="w-full h-full flex">
@@ -71,39 +31,7 @@ const Friends = ({ user }) => {
         </div>
       </div>
 
-      {loading ? (
-        <p>Loading...</p>
-      ) : (
-        <div className="flex flex-col">
-          <div className="w-full flex flex-col justify-center items-center gap-2">
-            <p className="text-2xl font-semibold">Add Friend</p>
-            <div className="flex">
-              <input
-                type="text"
-                placeholder="Start typing..."
-                onChange={(e) => {
-                  setFriendName(e.target.value);
-                }}
-              />
-              <button type="submit" className="btn" onClick={handleAddFriend}>
-                Add Friend
-              </button>
-            </div>
-          </div>
-          <div className="w-96 h-full flex flex-col justify-evenly items-center">
-            <p className="text-xl text-center font-semibold">Incoming Requests</p>
-            {requests.incoming &&
-              requests.incoming.map((request) => (
-                <RequestBar friendId={request.id} friendName={request.name} type="received" />
-              ))}
-            <p className="text-xl text-center font-semibold">Outgoing Requests</p>
-            {requests.outgoing &&
-              requests.outgoing.map((request) => (
-                <RequestBar friendId={request.id} friendName={request.name} type="sent" />
-              ))}
-          </div>
-        </div>
-      )}
+      <Requests/>
     </div>
   );
 };
--- apps/client/src/pages/Friends/friendBar.jsx
+++ apps/client/src/pages/Friends/friendBar.jsx
@@ -1,16 +1,62 @@
-const FriendBar = ({ friend }) => {
-  return (
-    <div>
-      <div className="flex flex-row justify-between items-center gap-4">
-        <p className="text-xl">{friend.username}</p>
-        {friend.owes >= 0 ? (
-          <span className="text-red-500">you owe {friend.owes}</span>
-        ) : (
-          <span className="text-green-500">owes you {-friend.owes}</span>
-        )}
+import { useState , useEffect } from "react";
+import axios from "axios";
+const FriendBar = ({friend})=>{
+    
+    const username = JSON.parse(localStorage.getItem("user")).username;
+    const [expenses , setExpenses] = useState([]);
+  
+
+        const fetch = async() => {
+            if(!expenses.length){
+            const res = await axios.post(`${import.meta.env.VITE_API_URL}/expense/get-common/${username}`,{
+                friend : friend.username
+
+            }, {
+                withCredentials: true,
+            });
+            console.log(res.data);
+            setExpenses(res.data);}
+        }
+
+
+    return(
+        <div className="collapse">
+        <input type="checkbox" 
+            onClick={fetch}
+        />
+        <div className="collapse-title text-xl font-medium">
+        <div className="flex flex-row justify-between items-center gap-4">
+                        <p className="text-xl">{friend.username}</p>
+                         {friend.owes >= 0 ?
+                        <span className="text-red-500">you owe {friend.owes}</span> :
+                        <span className="text-green-500">owes you {-friend.owes}</span>
+                        }
+                    </div>
+        </div>
+        <div className="collapse-content bg-black w-auto">
+        <div className="h-full flex flex-col justify-center items-center gap-4 flex-wrap">
+        {expenses.map((items) => {
+            const date = new Date(items.date).toLocaleString('en-IN' , {day : 'numeric' , month : 'short' , year : '2-digit'});
+            return (
+                        // a horizontal tab showing details in 1 line
+            <div className = "flex gap-2 justify-start">
+                <span>{date}</span>
+                <span> {items.name} </span>
+                {
+                    items.payer_id === friend.frnd_id ?
+                    <span className="text-red-500 self-end"> - {items.owe_amount}</span> :
+                    <span className="text-green-500 self-end">you lent {items.owe_amount}</span>
+                }
+
+            </div>
+                );
+        })}
       </div>
-    </div>
-  );
-};
+        </div>
+        </div>
+
+            
+            )
+}
 
 export default FriendBar;
