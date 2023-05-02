54e045f86f18a4b26b073223fe4306ab92113841
Allen Shibu <93600615+alapanoski@users.noreply.github.com>
2023-05-02 21:08:43 +0530

    File reorganization

---
+++ apps/client/src/pages/Dashboard/ExpenseExplorer.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react';
+
+import axios from 'axios';
+
+const ExpenseExplorer = ({ user }) => {
+  const [expenses, setExpenses] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${import.meta.env.VITE_API_URL}/expense/getall/user1`, {
+        withCredentials: true,
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
+        username: user.username,
+      })
+      .then((res) => {
+        console.log(res.data);
+        setExpenses(res.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      })
+      .finally(() => {});
+
+    console.log(user);
+  }, []);
+
+  return (
+    <div className="h-full flex flex-row justify-center items-center gap-4 flex-wrap">
+      {expenses.map((items) => {
+        return (
+          <div className="bg-black h-64 w-48 p-4 flex flex-col justify-evenly items-center rounded" key={items.exp_id}>
+            <div className="text-xl">{items.name}</div>
+            <div className="text-2xl font-bold">Rs. {items.owe_amount}</div>
+            <div className="">{items.c_name}</div>
+            <div className="text-xs">{new Date(items.date).toLocaleString().toString()}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default ExpenseExplorer;
