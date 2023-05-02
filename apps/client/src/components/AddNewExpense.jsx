820ffb664d26ca910eea1ea5062c65bccf1ac32b
Allen Shibu <93600615+alapanoski@users.noreply.github.com>
2023-05-02 21:32:29 +0530

    Add split feature

--- apps/client/src/components/AddNewExpense.jsx
+++ apps/client/src/components/AddNewExpense.jsx
@@ -1,12 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import axios from 'axios';
 
 const AddNewExpense = ({ user }) => {
-
   const username = JSON.parse(localStorage.getItem('user')).username;
   const [amount, setAmount] = useState(0);
   const [name, setName] = useState('');
+  const [isSplit, setIsSplit] = useState(false);
+  const [friends, setFriends] = useState([]);
+  const [selectedFriend, setSelectedFriend] = useState('');
+  const [splitFriends, setSplitFriends] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${import.meta.env.VITE_API_URL}/friends/getFriends/${username}`, {
+        withCredentials: true,
+      })
+      .then((res) => {
+        console.log(res.data);
+        setFriends(res.data);
+      });
+  }, [isSplit]);
 
   const addNewExpense = async () => {
     const expense = {
@@ -21,12 +35,29 @@ const AddNewExpense = ({ user }) => {
         },
       ],
     };
+
+    if (isSplit) {
+      splitFriends.forEach((friend) => {
+        expense.group.push({
+          username: friend,
+          amount: amount / (splitFriends.length + 1),
+        });
+      });
+    }
+
     console.log({ expense, user });
 
     const response = await axios.post(`${import.meta.env.VITE_API_URL}/expense/add/${username}`, {
       username: user.username,
       expense,
     });
+  };
+
+  const addFriendToSplit = () => {
+    if (selectedFriend === '') return;
+    if (splitFriends.includes(selectedFriend)) return;
+    setSplitFriends([...splitFriends, selectedFriend]);
+    console.log(splitFriends);
   };
 
   return (
@@ -46,9 +77,34 @@ const AddNewExpense = ({ user }) => {
           setAmount(e.target.value);
         }}
       />
-      <button className="btn" onClick={addNewExpense}>
-        Add
-      </button>
+
+      {isSplit ? (
+        <>
+          <select
+            name="split-friends"
+            id="split-friends"
+            onChange={(e) => {
+              setSelectedFriend(e.target.value);
+              console.log(selectedFriend);
+            }}
+          >
+            {friends.map((friend) => {
+              return <option value={friend.username}>{friend.username}</option>;
+            })}
+          </select>
+          <button className="btn" onClick={addFriendToSplit}>
+            Add Friend
+          </button>
+        </>
+      ) : null}
+      <div className="flex flex-row gap-2">
+        <button className="btn" onClick={() => setIsSplit(!isSplit)}>
+          Split
+        </button>
+        <button className="btn" onClick={addNewExpense}>
+          Add
+        </button>
+      </div>
     </div>
   );
 };
