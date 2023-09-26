9107aef12526c061a34e2efee49a71f57faed2bb
Allen Shibu <93600615+alllenshibu@users.noreply.github.com>
2023-09-26 20:42:06 +0530

    Uncomment lines

--- apps/server/src/middlewares/auth.middleware.js
+++ apps/server/src/middlewares/auth.middleware.js
@@ -1,24 +1,29 @@
 const pool = require("../utils/pg");
 
 const authorize = async (req, res, next) => {
-    const authHeader = req.headers.authorization;
+    try {
+        const authHeader = req.headers.authorization;
 
-    // if (!authHeader || authHeader === '' || authHeader === undefined) {
-    //     return res.status(401).send('Unauthorized');
-    // }
-    // const token = authHeader.split(' ')[1];
-    // if (!token || token === '' || token === undefined) {
-    //     return res.status(401).send('Unauthorized');
-    // }
+        if (!authHeader || authHeader === '' || authHeader === undefined) {
+            return res.status(401).send('Unauthorized');
+        }
+        const token = authHeader.split(' ')[1];
+        if (!token || token === '' || token === undefined) {
+            return res.status(401).send('Unauthorized');
+        }
 
-    // const user = await pool.query('SELECT * FROM "user" WHERE id = $1', [token]);
+        const user = await pool.query('SELECT * FROM "user" WHERE id = $1', [token]);
 
-    // if (user?.rows?.length === 0) {
-    //     return res.status(401).send('Unauthorized');
-    // }
+        if (user?.rows?.length === 0) {
+            return res.status(401).send('Unauthorized');
+        }
 
-    req.user = user?.rows[0]?.email;
-    next();
+        req.user = user?.rows[0]?.email;
+        next();
+    } catch (err) {
+        console.log(err);
+        return res.status(401).send('Unauthorized');
+    }
 }
 
 module.exports = {
