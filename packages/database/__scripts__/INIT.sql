aeb8bf8384ff4ed31bddaca74ae3bc4ec5fe9e60
Allen Shibu <93600615+alllenshibu@users.noreply.github.com>
2023-11-10 21:36:43 +0530

    Change in schema

--- packages/database/__scripts__/INIT.sql
+++ packages/database/__scripts__/INIT.sql
@@ -16,21 +16,21 @@ CREATE TABLE IF NOT EXISTS "user"
 (
     id         UUID DEFAULT uuid_generate_v4(),
 
-    first_name VARCHAR(32)    NOT NULL,
-    last_name  VARCHAR(32)    NOT NULL,
-    email      VARCHAR(32)    NOT NULL,
-    password   VARCHAR(256)   NOT NULL,
-    budget     NUMERIC(10, 2),
+    first_name VARCHAR(32)  NOT NULL,
+    last_name  VARCHAR(32)  NOT NULL,
+    email      VARCHAR(32)  NOT NULL,
+    password   VARCHAR(256) NOT NULL,
 
     PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS payment_option
 (
-    id      UUID DEFAULT uuid_generate_v4(),
+    id       UUID                 DEFAULT uuid_generate_v4(),
 
-    user_id UUID        NOT NULL,
-    name    VARCHAR(32) NOT NULL,
+    user_id  UUID        NOT NULL,
+    name     VARCHAR(32) NOT NULL,
+    disabled BOOLEAN     NOT NULL DEFAULT FALSE,
 
     PRIMARY KEY (id),
     FOREIGN KEY (user_id) REFERENCES "user" (id)
@@ -64,22 +64,6 @@ CREATE TABLE IF NOT EXISTS expense
     FOREIGN KEY (user_id) REFERENCES "user" (id),
     FOREIGN KEY (category_id) REFERENCES category (id),
     FOREIGN KEY (payment_id) REFERENCES payment_option (id)
-);
-
-CREATE TABLE IF NOT EXISTS recurring_payment
-(
-    id          UUID DEFAULT uuid_generate_v4(),
-
-    user_id     UUID           NOT NULL,
-    category_id UUID,
-
-    title       VARCHAR(32)    NOT NULL,
-    amount      NUMERIC(10, 2) NOT NULL,
-    ends_on     DATE           NOT NULL,
-
-    PRIMARY KEY (id),
-    FOREIGN KEY (user_id) REFERENCES "user" (id),
-    FOREIGN KEY (category_id) REFERENCES category (id)
 );
 
 CREATE TABLE IF NOT EXISTS friend_request
@@ -116,19 +100,34 @@ CREATE TABLE IF NOT EXISTS split
     FOREIGN KEY (user_id) REFERENCES "user" (id)
 );
 
-
 CREATE TABLE IF NOT EXISTS goal
 (
-    id      UUID DEFAULT uuid_generate_v4(),
+    id      UUID                    DEFAULT uuid_generate_v4(),
 
     user_id UUID           NOT NULL,
 
     month   INT            NOT NULL,
     year    INT            NOT NULL,
     goal    NUMERIC(10, 2) NOT NULL,
-    spent   NUMERIC(10, 2) NOT NULL,
+    spent   NUMERIC(10, 2) NOT NULL DEFAULT 0,
 
     PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS recurring_payment
+(
+    id          UUID DEFAULT uuid_generate_v4(),
+
+    user_id     UUID           NOT NULL,
+    category_id UUID,
+
+    title       VARCHAR(32)    NOT NULL,
+    amount      NUMERIC(10, 2) NOT NULL,
+    ends_on     DATE           NOT NULL,
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES "user" (id),
+    FOREIGN KEY (category_id) REFERENCES category (id)
 );
 
 -- INSERT INTO split (expense_id, user_id, percentage) VALUES ('02c5685c-eaef-40e5-bf12-b7d6dfe16c88','64acd7ab-fa45-46d3-a73b-05fd08745699', 100)
