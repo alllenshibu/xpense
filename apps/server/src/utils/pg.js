7e42e168a8873fcee52f0f3eae8a241c4c578bef
Allen Shibu <93600615+alllenshibu@users.noreply.github.com>
2023-11-11 14:55:36 +0530

    console error

--- apps/server/src/utils/pg.js
+++ apps/server/src/utils/pg.js
@@ -3,52 +3,57 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 function getPostgresCredentials() {
-    const user = process.env.POSTGRES_USER;
-    const password = process.env.POSTGRES_PASSWORD;
-    const host = process.env.POSTGRES_HOST;
-    const port = Number(process.env.POSTGRES_PORT);
-    const database = process.env.POSTGRES_DATABASE;
+  const user = process.env.POSTGRES_USER;
+  const password = process.env.POSTGRES_PASSWORD;
+  const host = process.env.POSTGRES_HOST;
+  const port = Number(process.env.POSTGRES_PORT);
+  const database = process.env.POSTGRES_DATABASE;
 
-    if (!user || user === '' || user === undefined) {
-        throw new Error('Cannot find POSTGRES_USER');
-    }
+  if (!user || user === '' || user === undefined) {
+    console.error('Cannot find POSTGRES_USER');
+    throw new Error('Cannot find POSTGRES_USER');
+  }
 
-    if (!password || password === '' || password === undefined) {
-        throw new Error('Cannot find POSTGRES_PASSWORD');
-    }
+  if (!password || password === '' || password === undefined) {
+    console.error('Cannot find POSTGRES_PASSWORD');
+    throw new Error('Cannot find POSTGRES_PASSWORD');
+  }
 
-    if (!host || host === '' || host === undefined) {
-        throw new Error('Cannot find POSTGRSE_HOST');
-    }
+  if (!host || host === '' || host === undefined) {
+    console.error('Cannot find POSTGRES_HOST');
+    throw new Error('Cannot find POSTGRSE_HOST');
+  }
 
-    if (!port || port === 0 || port === undefined) {
-        throw new Error('Cannot find POSTGRES_PORT');
-    }
+  if (!port || port === 0 || port === undefined) {
+    console.error('Cannot find POSTGRES_PORT');
+    throw new Error('Cannot find POSTGRES_PORT');
+  }
 
-    if (!database || database === '' || database === undefined) {
-        throw new Error('Cannot find POSTGRES_DATABASE');
-    }
+  if (!database || database === '' || database === undefined) {
+    console.error('Cannot find POSTGRES_DATABASE');
+    throw new Error('Cannot find POSTGRES_DATABASE');
+  }
 
-    console.log("Connected to database")
+  console.log('Connected to database');
 
-    return {
-        user,
-        password,
-        host,
-        port,
-        database,
-    };
+  return {
+    user,
+    password,
+    host,
+    port,
+    database,
+  };
 }
 
 const pool = new Pool({
-    user: getPostgresCredentials().user,
-    password: getPostgresCredentials().password,
-    host: getPostgresCredentials().host,
-    port: getPostgresCredentials().port,
-    database: getPostgresCredentials().database,
-    // ssl: {
-    //     rejectUnauthorized: false,
-    // },
+  user: getPostgresCredentials().user,
+  password: getPostgresCredentials().password,
+  host: getPostgresCredentials().host,
+  port: getPostgresCredentials().port,
+  database: getPostgresCredentials().database,
+  // ssl: {
+  //     rejectUnauthorized: false,
+  // },
 });
 
 module.exports = pool;
