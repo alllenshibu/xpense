440e55457f82ebdf6a7cf47f0705bdf59ab9c0f8
Allen Shibu <93600615+alllenshibu@users.noreply.github.com>
2023-11-13 15:24:17 +0000

    category in expense add

--- apps/client/src/pages/expense/new/index.js
+++ apps/client/src/pages/expense/new/index.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 
 import axiosInstance from '@/lib/axiosInstance';
 
 import ExpenseEditor from '@/components/ExpenseEditor';
 import DashboardLayout from '@/layouts/DashboardLayout';
+import { fetchAllCategories } from '@/services/category';
 
 export default function AddNewExpense() {
   const router = useRouter();
@@ -12,8 +13,11 @@ export default function AddNewExpense() {
   const [expense, setExpense] = useState({
     title: '',
     amount: '',
+    categoryId: '',
     timestamp: new Date().toISOString().slice(0, 16),
   });
+
+  const [categories, setCategories] = useState([]);
 
   const handleSubmit = async (e) => {
     try {
@@ -33,12 +37,28 @@ export default function AddNewExpense() {
     }
   };
 
+  const fetchCategories = async () => {
+    const res = await fetchAllCategories();
+    if (res.status === 200) {
+      setCategories(res.data.categories);
+    } else if (res.status === 500) {
+      alert('Something went wrong with the server');
+    } else {
+      alert('Something went wrong');
+    }
+  };
+
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+
   return (
     <DashboardLayout>
       <div className="h-full w-full flex items-center justify-center">
         <ExpenseEditor
           expense={expense}
           setExpense={setExpense}
+          categories={categories}
           submitText={'Add'}
           handleSubmit={handleSubmit}
         />
