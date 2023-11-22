9d93811dd4f45cda07b27549d6097608cc551f0c
Allen Shibu <93600615+alllenshibu@users.noreply.github.com>
2023-11-22 23:40:06 +0530

    Trigger for category

--- packages/database/__scripts__/INIT.sql
+++ packages/database/__scripts__/INIT.sql
@@ -2,6 +2,9 @@ DROP EXTENSION IF EXISTS "uuid-ossp" CASCADE;
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
+DROP TRIGGER def_cats ON "user" CASCADE;
+DROP FUNCTION create_def_cats() CASCADE;
+
 DROP TABLE IF EXISTS "user" CASCADE;
 DROP TABLE IF EXISTS payment_option CASCADE;
 DROP TABLE IF EXISTS category CASCADE;
@@ -19,55 +22,40 @@ CREATE TABLE IF NOT EXISTS "user"
 
     first_name VARCHAR(32)  NOT NULL,
     last_name  VARCHAR(32)  NOT NULL,
-    email      VARCHAR(32)  NOT NULL,   
+    email      VARCHAR(32)  NOT NULL,
     password   VARCHAR(256) NOT NULL,
 
     PRIMARY KEY (id)
 );
-    
-CREATE OR REPLACE FUNCTION add_default_category_and_payment()
-RETURNS TRIGGER AS $$
-BEGIN
-    -- Add default category
-    BEGIN
-        INSERT INTO category (user_id, name)
-        VALUES (NEW.id, '(default)');
-        INSERT INTO category (user_id, name)
-        VALUES (NEW.id, 'Groceries');
-        INSERT INTO category (user_id, name)
-        VALUES (NEW.id, 'Entertainment');
-    EXCEPTION
-        WHEN others THEN
-            RAISE NOTICE 'Error adding default category: %', SQLERRM;
-    END;
+  CREATE OR REPLACE FUNCTION create_def_cats()
+      RETURNS TRIGGER AS
+  $$
+  BEGIN
+      INSERT INTO category (user_id, name)
+      VALUES (NEW.id, '(default)');
 
-    -- Add default payment option
-    BEGIN
-        INSERT INTO payment_option (user_id, name)
-        VALUES (NEW.id, '(default)');
-    EXCEPTION
-        WHEN others THEN
-            RAISE NOTICE 'Error adding default payment option: %', SQLERRM;
-    END;
+      INSERT INTO category (user_id, name)
+      VALUES (NEW.id, 'Groceries');
 
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
+      INSERT INTO category (user_id, name)
+      VALUES (NEW.id, 'Entertainment');
 
+      RETURN NEW;
+  END;
+  $$ LANGUAGE plpgsql;
 
--- Create a trigger to invoke the function on user creation
-CREATE TRIGGER add_default_category_and_payment_trigger
-AFTER INSERT
-ON "user"
-FOR EACH ROW
-EXECUTE FUNCTION add_default_category_and_payment();
+  CREATE TRIGGER def_cats
+      AFTER INSERT
+      ON "user"
+      FOR EACH ROW
+  EXECUTE FUNCTION create_def_cats();
 
 
 
 CREATE TABLE IF NOT EXISTS payment_option
 (
     id       UUID                 DEFAULT uuid_generate_v4(),
-    
+
     user_id  UUID        NOT NULL,
     name     VARCHAR(32) NOT NULL,
     disabled BOOLEAN     NOT NULL DEFAULT FALSE,
@@ -142,11 +130,11 @@ CREATE TABLE IF NOT EXISTS split
 
 CREATE TABLE IF NOT EXISTS income
 (
-    id         UUID DEFAULT uuid_generate_v4(),
+    id        UUID DEFAULT uuid_generate_v4(),
 
-    user_id    UUID          NOT NULL,
-    amount     NUMERIC(10, 2) NOT NULL,
-    timestamp  TIMESTAMPTZ    NOT NULL,
+    user_id   UUID           NOT NULL,
+    amount    NUMERIC(10, 2) NOT NULL,
+    timestamp TIMESTAMPTZ    NOT NULL,
 
     PRIMARY KEY (id),
     FOREIGN KEY (user_id) REFERENCES "user" (id)
