70c7a7869661bdf5d47cd8392bad1a9ad84b33bf
Allen Shibu <93600615+alllenshibu@users.noreply.github.com>
2023-12-28 18:35:22 +0530

    Fix google analytics

--- apps/client/src/pages/_app.js
+++ apps/client/src/pages/_app.js
@@ -1,6 +1,7 @@
 import '@/styles/globals.scss';
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }) {
   const router = useRouter();
@@ -16,11 +17,14 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       {/* Google tag (gtag.js) */}
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-NGDNMMTPP5"></script>
-      <script>
-        window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments)}
-        gtag('js', new Date()); gtag('config', process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID);
-      </script>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-NGDNMMTPP5" />
+      <Script id="google-analytics">
+        {`
+            window.dataLayer = window.dataLayer || []; 
+            function gtag(){dataLayer.push(arguments)}
+            gtag('js', new Date()); gtag('config', process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID);
+        `}
+      </Script>
       <Component {...pageProps} />;
     </>
   );
